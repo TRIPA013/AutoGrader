b63d79fe06d08e21f0cbdef4bc78ac5ab7082135
TRIPA013 <abhinav.tripathi@disney.com>
2014-10-22 22:02:06 -0400

    Pojo Objects

--- AutoGrader/src/com/zzat/autograder/pojo/Assignment.java
+++ AutoGrader/src/com/zzat/autograder/pojo/Assignment.java
@@ -2,4 +2,76 @@ package com.zzat.autograder.pojo;
 
 public class Assignment {
 
+	private int courseID;
+
+	public enum AssignmentType {
+		HOMEWORK, QUIZ, EXAM;
+	}
+
+	private AssignmentType assignmentType;
+
+	private double maxPoints;
+
+	private String configurationFile;
+
+	/**
+	 * @return the courseID
+	 */
+	public int getCourseID() {
+		return courseID;
+	}
+
+	/**
+	 * @param courseID
+	 *            the courseID to set
+	 */
+	public void setCourseID(int courseID) {
+		this.courseID = courseID;
+	}
+
+	/**
+	 * @return the assignmentType
+	 */
+	public AssignmentType getAssignmentType() {
+		return assignmentType;
+	}
+
+	/**
+	 * @param assignmentType
+	 *            the assignmentType to set
+	 */
+	public void setAssignmentType(AssignmentType assignmentType) {
+		this.assignmentType = assignmentType;
+	}
+
+	/**
+	 * @return the maxPoints
+	 */
+	public double getMaxPoints() {
+		return maxPoints;
+	}
+
+	/**
+	 * @param maxPoints
+	 *            the maxPoints to set
+	 */
+	public void setMaxPoints(double maxPoints) {
+		this.maxPoints = maxPoints;
+	}
+
+	/**
+	 * @return the configurationFile
+	 */
+	public String getConfigurationFile() {
+		return configurationFile;
+	}
+
+	/**
+	 * @param configurationFile
+	 *            the configurationFile to set
+	 */
+	public void setConfigurationFile(String configurationFile) {
+		this.configurationFile = configurationFile;
+	}
+
 }
